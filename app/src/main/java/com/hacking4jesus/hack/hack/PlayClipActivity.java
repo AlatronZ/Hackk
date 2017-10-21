515e44dec4baab8708c04e9bdbb7af9c4ab2d217
David Peck <ExplosiveCrayonz@gmail.com>
2017-10-22 03:53:41 +1300

    checkpoint layout added

--- app/src/main/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
+++ app/src/main/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
@@ -2,15 +2,19 @@ package com.hacking4jesus.hack.hack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class PlayClipActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-    }
 
-    public void onPlay(){
+        RelativeLayout view = (RelativeLayout)LayoutInflater.from(this).inflate(R.layout.activity_start, null);
+        LinearLayout checkpoints = (LinearLayout)view.findViewById(R.id.checkpoints);
 
     }
 }
