aa233539566bc164b1a5f335e8cb2c871aeff089
David Peck <ExplosiveCrayonz@gmail.com>
2017-10-21 21:01:41 +1300

    the stuff

---
+++ app/src/main/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
@@ -0,0 +1,12 @@
+package com.hacking4jesus.hack.hack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlayClipActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
