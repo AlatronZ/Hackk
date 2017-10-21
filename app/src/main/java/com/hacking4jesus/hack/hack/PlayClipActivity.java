9a931a59ed9afae4251a7c65fa30238eb55cc9d7
David Peck <ExplosiveCrayonz@gmail.com>
2017-10-22 10:22:54 +1300

    activity layout updates

--- app/src/main/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
+++ app/src/main/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
@@ -2,9 +2,6 @@ package com.hacking4jesus.hack.hack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 public class PlayClipActivity extends AppCompatActivity {
 
@@ -12,9 +9,5 @@ public class PlayClipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        RelativeLayout view = (RelativeLayout)LayoutInflater.from(this).inflate(R.layout.activity_start, null);
-        LinearLayout checkpoints = (LinearLayout)view.findViewById(R.id.checkpoints);
-
     }
 }
