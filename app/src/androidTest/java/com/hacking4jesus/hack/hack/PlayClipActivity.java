576c951f516e4b46524d9635d0a6712973da1ea4
Alastair <AlatronZ@users.noreply.github.com>
2017-10-22 14:16:00 +1300

    Add files via upload
    PlayClip java push

---
+++ app/src/androidTest/java/com/hacking4jesus/hack/hack/PlayClipActivity.java
@@ -0,0 +1,24 @@
+package com.hacking4jesus.hack.hack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlayClipActivity extends AppCompatActivity {
+//    Button videoLeftButton = (Button) findViewById(R.id.buttonLeft);
+//    videoLeftButton.setOnClickListener(new onClickListener(){
+//        public void onClick(View v){
+//            //go back, GET TO DA CHOPPER
+//        }
+//    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void onPlay(){
+
+    }
+
+}
